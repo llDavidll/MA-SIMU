c21af57e017bdb99425c55171b285e9f8c2be075
llDavidll <d.lericolais@gmail.com>
2014-04-18 10:35:28 +0200

    Added a JPanel to the JFrame to stop the blinking of the view.

--- src/applicationSimulateur/VueVoiture.java
+++ src/applicationSimulateur/VueVoiture.java
@@ -5,9 +5,9 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class VueVoiture extends JFrame {
@@ -25,37 +25,42 @@ public class VueVoiture extends JFrame {
 		this.setTitle("Simulateur de Voiture");
 		this.setSize(TailleFenetreEnPixels, TailleFenetreEnPixels);
 		this.setVisible(true);
+		this.add(new VueVoiturePanel());
 		this.voiture = new Triangle2D();
 		this.controleurVoiture = controleurVoiture;
 		this.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				controleurVoiture.inputs(e);
-				
+				VueVoiture.this.controleurVoiture.inputs(e);
+
 			}
 		});
 	}
 
-	@Override
-	public void paint(Graphics graphics) {
-		super.paint(graphics);
-		dessinerVoiture(graphics);
-
-	}
-
-	public void dessinerVoiture(Graphics graphics) {
-
-		Graphics2D g2d = (Graphics2D) graphics;
-
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-
-		int[] xPoints = { voiture.getAvant().getX(), voiture.getAd().getX(), voiture.getAg().getX() };
-		int[] yPoints = { voiture.getAvant().getY(), voiture.getAd().getY(), voiture.getAg().getY() };
-		graphics.fillPolygon(xPoints, yPoints, 3);
-	}
-
 	public void setVoiture(int xVoiture, int yVoiture, double angle) {
-		this.voiture = new Triangle2D(new Vector2D(xVoiture,yVoiture),angle);
+		this.voiture = new Triangle2D(new Vector2D(xVoiture, yVoiture), angle);
+	}
+
+	private class VueVoiturePanel extends JPanel {
+
+		@Override
+		protected void paintComponent(Graphics graphics) {
+			super.paintComponent(graphics);
+			dessinerVoiture(graphics);
+		}
+
+		public void dessinerVoiture(Graphics graphics) {
+
+			Graphics2D g2d = (Graphics2D) graphics;
+
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+
+			int[] xPoints = { voiture.getAvant().getX(),
+					voiture.getAd().getX(), voiture.getAg().getX() };
+			int[] yPoints = { voiture.getAvant().getY(),
+					voiture.getAd().getY(), voiture.getAg().getY() };
+			graphics.fillPolygon(xPoints, yPoints, 3);
+		}
 	}
 }
