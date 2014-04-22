f93afe3fb6f1a83b9d7cfe2f46dac7a07157d7a8
laurineTP <laurine.marmisse@univ-tlse3.fr>
2014-04-22 09:47:46 +0200

    Test vue voiture

--- src/tests/TestVueVoiture.java
+++ src/tests/TestVueVoiture.java
@@ -1,5 +1,29 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domaineConduite.Voiture;
+import applicationSimulateur.ControleurVoiture;
+import applicationSimulateur.VueVoiture;
+
 public class TestVueVoiture {
+
+	VueVoiture vueVoiture;
+	
+	@Before
+	public void setUp() throws Exception {
+		Voiture voiture = new Voiture(100, 900, 20);
+		ControleurVoiture controleurVoiture;
+
+		controleurVoiture = new ControleurVoiture(voiture);
+		vueVoiture = new VueVoiture(controleurVoiture);
+	}
+
+	@Test
+	public void testSetVoiture() {
+	}
 
 }
